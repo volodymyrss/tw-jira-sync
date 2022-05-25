806cc281063fd018e8db49e1845ac3ea86e4cf6d
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2022-05-25 18:05:28 +0200

    use uuid

--- tjs/cli.py
+++ tjs/cli.py
@@ -15,7 +15,10 @@ import subprocess
 
 from tjs.utils import duration_to_seconds
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('tw-jira-sync')
+
+class DuplicateIssue(Exception):
+    pass
 
 class TaskWarriorJIRA(JIRA):
     # overrides to assume project
@@ -33,11 +36,11 @@ class TaskWarriorJIRA(JIRA):
     def search_issues(self, jql_str: str, *args, **kwargs):
         return super().search_issues(f"project={self.project_name} AND " + jql_str, *args, **kwargs)
 
-    def create_issue(self, taskid: int, fields: Optional[Dict[str, Any]] = None, prefetch: bool = True, **fieldargs) -> Issue:        
+    def create_issue(self, taskuuid: str, fields: Optional[Dict[str, Any]] = None, prefetch: bool = True, **fieldargs) -> Issue:        
         return super().create_issue(fields, 
                                     prefetch,
                                     project=self.project_name,
-                                    customfield_10035 = taskid,
+                                    customfield_10045 = taskuuid,
                                     **fieldargs)            
 
     # -----
@@ -48,39 +51,50 @@ class TaskWarriorJIRA(JIRA):
 
     def load_cache(self):        
         try:
-            self.cache_by_taskid = pickle.load(open(self.cache_fn, "rb"))
+            self.cache_by_taskuuid = pickle.load(open(self.cache_fn, "rb"))
         except Exception as e:
-            self.cache_by_taskid = {}
+            self.cache_by_taskuuid = {}
 
     def reset_cache(self):
-        self.cache_by_taskid = {}
+        self.cache_by_taskuuid = {}
         self.write_cache()
 
     def write_cache(self):        
         os.makedirs(os.path.dirname(self.cache_fn), exist_ok=True)
-        pickle.dump(self.cache_by_taskid, open(self.cache_fn, "wb"))
+        pickle.dump(self.cache_by_taskuuid, open(self.cache_fn, "wb"))
     
 
-    def issue_for_taskid(self, taskid: int, use_cache=False):
-        logger.debug('will find issue_for_taskid')
+    def issue_for_taskuuid(self, taskuuid: str, use_cache=False):
+        logger.debug('will find issue_for_taskuuid')
         if use_cache:
-            if taskid in self.cache_by_taskid:
-                logger.debug("found in cache: %s, %s", taskid, self.cache_by_taskid[taskid])            
-                return self.cache_by_taskid[taskid]
+            if taskuuid in self.cache_by_taskuuid:
+                logger.debug("found in cache: %s, %s", taskuuid, self.cache_by_taskuuid[taskuuid])            
+                return self.cache_by_taskuuid[taskuuid]
                 
-        issues = [i for i in self.search_issues(f'TaskWarriorID = {taskid}')]
+        issues = [i for i in self.search_issues(f'TaskWarriorUUID ~ "{taskuuid}" OR description ~ "uuid: {taskuuid}"')]
+        
 
         if len(issues) > 1:
-            print("found many!", taskid, issues)            
-            raise NotImplementedError
+            logger.error("found many task for uuid %s : %s", taskuuid, issues)            
+            for issue in issues:
+                logger.error("issue: https://odahub.atlassian.net/browse/%s", issue.key)
+
+            raise DuplicateIssue()
         elif len(issues) == 0:
-            print("found none!", taskid, issues)            
+            print("found none!", taskuuid, issues)            
             return None
         else:
-            print("found this:", taskid, issues)            
-            self.cache_by_taskid[taskid] = issues[0]
+            print("found this:", taskuuid, issues)            
+            self.cache_by_taskuuid[taskuuid] = issues[0]
             self.write_cache()
             return issues[0]
+
+
+class Formatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        # for k, v in record.__dict__.items():
+        #     print(k, ":", v)
+        return super().format(record)
 
 
 @click.group()
@@ -89,8 +103,20 @@ class TaskWarriorJIRA(JIRA):
 @click.option('-R', '--reset-cache', is_flag=True)
 @click.pass_obj
 def cli(obj, project, verbose, reset_cache):
-    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
+    formatter = Formatter('\033[02;32m%(asctime)s\033[0m - %(name)25s - %(levelname)10s - %(message)s') #
+    
+    if verbose:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.INFO
+    
+    logging.basicConfig(
+        level=loglevel
+    )
 
+    for h in logging.getLogger().handlers:        
+        h.setFormatter(formatter)
+        
     obj['auth_jira'] = TaskWarriorJIRA(
                             'https://odahub.atlassian.net', 
                             project_name=project,
@@ -110,15 +136,17 @@ def print_issue(issue, long):
                 print('    \033[36m{}\033[0m: {}'.format(k, v))
 
         for issue_link in issue.fields.issuelinks:
-            print(f"{issue_link}: {issue_link.type} {issue_link.outwardIssue}")
+            print(f"{issue_link}: {issue_link.type} => {getattr(issue_link, 'outwardIssue', 'UNSET')}")
             # for k in dir(issue_link):
             #     print(f" > {k} {getattr(issue_link, k)}")
+        
+        
             
 @cli.command()
-@click.option('-i', '--task-id', type=int, default=None)
+@click.option('-i', '--task-uuid', 'taskuuid', type=str, default=None)
 @click.option("-l", "--long", is_flag=True)
 @click.pass_obj
-def list(obj, long, task_id):
+def list(obj, long, taskuuid):
     jira = obj['auth_jira']
 
     # Search returns first 50 results, `maxResults` must be set to exceed this
@@ -131,8 +159,8 @@ def list(obj, long, task_id):
     # Summaries of my last 3 reported issues
 
     extra_filter = ''
-    if task_id is not None:
-        extra_filter =  f'AND TaskWarriorID = {task_id}'
+    if taskuuid is not None:
+        extra_filter =  f'AND TaskWarriorUUID = {taskuuid}'
 
     for issue in jira.search_issues(f'{extra_filter} order by created desc ', maxResults=False):
         print_issue(issue, long)
@@ -146,12 +174,12 @@ def list(obj, long, task_id):
 
             
 @cli.command("push")
-@click.option('-i', '--task-id', type=int)
+@click.option('-i', '--task-uuid', 'taskuuid', type=str)
 @click.option("-l", "--long", is_flag=True)
 @click.option("-U", "--allow-update", is_flag=True)
 @click.option("-1", "--run-once", is_flag=True)
 @click.pass_obj
-def _push(obj, task_id, long, allow_update, run_once):
+def _push(obj, taskuuid, long, allow_update, run_once):
     jira = obj['auth_jira']
 
     # print(json.dumps(jira.createmeta(
@@ -161,7 +189,7 @@ def _push(obj, task_id, long, allow_update, run_once):
     #         sort_keys=True))
 
     while True:
-        push(jira, task_id, long, allow_update)
+        push(jira, taskuuid, long, allow_update)
 
         if run_once:
             break
@@ -173,102 +201,117 @@ def _push(obj, task_id, long, allow_update, run_once):
                 
 
 
-def push(jira: TaskWarriorJIRA, task_id, long, allow_update):
+def push(jira: TaskWarriorJIRA, taskuuid: str, long: bool, allow_update: bool):
     w = TaskWarrior()
     tasks = w.load_tasks()
-    # for k, v in tasks.items():
-    #     print(k)
-    for task in tasks['pending']:
-        # print(task)
-        taskid = task["id"]
 
-        if task_id is not None and task_id != taskid:
+    for k, v in tasks.items():
+        logger.info('found %s : %s', k, len(v))
+
+    duplicate_issues = []
+    
+    for task in tasks['pending']:
+        
+        if taskuuid is not None and task["uuid"] != taskuuid:
             continue
 
-        issuetype = 'Task'
-        for issuetype_key in ['redminetracker']:
-            if issuetype_key in task:
-                issuetype = task[issuetype_key]        
-                logger.debug('deduced issuetype %s from %s', issuetype_key, issuetype)
-
-                
-        issue = jira.issue_for_taskid(taskid, use_cache=not allow_update)
-                
-        if issue is not None:
-            if not allow_update:
-                logger.debug("skipping updates!")
-                continue
-                
-        else:
-            print("NOT found, will create")
-            issue = jira.create_issue(
-                    taskid=taskid,
-                    summary=task['description'],                    
-                    issuetype={'name': issuetype},
-                )
-
-            print('created: {}: {}'.format(issue.key, issue.fields.summary))
-        
-
-        # title 
-
-        for title_key in ['redminesubject', 'gitlabtitle']:
-            if title_key in task:
-                print('updating title')
-                task['description'] = task[title_key]
-
-        # base
+        try:
+            push_task(jira, task, allow_update, long)
+        except DuplicateIssue:
+            logger.error('duplicate issue for %s', task['uuid'])
+            duplicate_issues.append(task)
 
 
-        fields = dict(
-                    summary=task['description'],
-                    description="\n".join([f"{k}: {v}" for k, v in task.items()]), 
-                    labels=None
-                )
 
-        # estimate
+def push_task(jira, task, allow_update, long):
+    issuetype = 'Task'
+    for issuetype_key in ['redminetracker']:
+        if issuetype_key in task:
+            issuetype = task[issuetype_key]        
+            logger.debug('deduced issuetype %s from %s', issuetype_key, issuetype)
 
-        estimate_s = None
-        for estimate_key in ['redmineestimatedhours']:
-            if estimate_key in task:
-                estimate_s = duration_to_seconds(task[estimate_key])
-                fields['timetracking'] = {
-                    "originalEstimate": f"{int(estimate_s/60.)}m"
-                }                
+            
+    issue = jira.issue_for_taskuuid(task["uuid"], use_cache=not allow_update)
+            
+    if issue is not None:                        
+        logger.info('found Jira issue %s for taskuuid=%s', issue, task["uuid"])
 
-        
-        issue.update(**fields) 
+        if long:
+            print_issue(issue, True)
 
-        # tags
-
-        tags = task.get('tags', [])
-
-        for tag_field in ['project', 'gitlabnamespace']:
-            if tag_field in task:
-                tags.append(task[tag_field])
-
-        print("updating labels", tags)
-
-        
-        for tag in tags:
-            issue.add_field_value(
-                'labels', tag
+        if not allow_update:
+            logger.debug("skipping updates!")
+            return
+            
+    else:
+        print("NOT found, will create")
+        issue = jira.create_issue(
+                taskuuid=task["uuid"],
+                summary=task['description'],                    
+                issuetype={'name': issuetype},
             )
 
-        # urls
+        print('created: {}: {}'.format(issue.key, issue.fields.summary))
+    
 
-        for url_field, title in [
-                ('redmineurl', "Redmine URL")
-            ]:
-            if url_field in task:
-                print('adding', task[url_field])
-                jira.add_simple_link(issue, {
-                    "url": task[url_field],
-                    "title": title
-                })
+    # title 
 
-        print_issue(issue, long)
-            
+    for title_key in ['redminesubject', 'gitlabtitle']:
+        if title_key in task:
+            print('updating title')
+            task['description'] = task[title_key]
+
+    # base
+
+
+    fields = dict(
+                summary=task['description'],
+                description="\n".join([f"{k}: {v}" for k, v in task.items()]), 
+                labels=None
+            )
+
+    # estimate
+
+    estimate_s = None
+    for estimate_key in ['redmineestimatedhours']:
+        if estimate_key in task:
+            estimate_s = duration_to_seconds(task[estimate_key])
+            fields['timetracking'] = {
+                "originalEstimate": f"{int(estimate_s/60.)}m"
+            }                
+
+    
+    issue.update(**fields) 
+
+    # tags
+
+    tags = task.get('tags', [])
+
+    for tag_field in ['project', 'gitlabnamespace']:
+        if tag_field in task:
+            tags.append(task[tag_field])
+
+    print("updating labels", tags)
+
+    
+    for tag in tags:
+        issue.add_field_value(
+            'labels', tag
+        )
+
+    # urls
+
+    for url_field, title in [
+            ('redmineurl', "Redmine URL")
+        ]:
+        if url_field in task:
+            print('adding', task[url_field])
+            jira.add_simple_link(issue, {
+                "url": task[url_field],
+                "title": title
+            })
+
+    print_issue(issue, long)
 
 
 # issue = jira.issue('JRA-9')
